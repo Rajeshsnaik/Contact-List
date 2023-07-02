07dcfb94ff234bb221bc38f861a2384ec6c39781
Rajesh Naik <93809175+Rajeshsnaik@users.noreply.github.com>
2023-07-02 11:53:24 +0530

    Add files via upload

---
+++ react-props/src/contacts.js
@@ -0,0 +1,31 @@
+const contacts = [
+  {
+    name: "Virat Kohli",
+    imgURL:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Virat_Kohli_during_the_India_vs_Aus_4th_Test_match_at_Narendra_Modi_Stadium_on_09_March_2023.jpg/330px-Virat_Kohli_during_the_India_vs_Aus_4th_Test_match_at_Narendra_Modi_Stadium_on_09_March_2023.jpg ",
+    phone: "+91 0000000000",
+    email: "virat@gmail.com"
+  },
+  {
+    name: "MS Dhoni",
+    imgURL:"https://bsmedia.business-standard.com/_media/bs/img/article/2019-07/12/full/1562945642-9975.jpg",
+    phone: "+91 0000000000",
+    email: "msdhoni@gmail.com"
+  },
+  {
+    name: "Yash",
+    imgURL:"https://m.media-amazon.com/images/M/MV5BNGJlOTljYmQtM2FkYS00YTEyLTliOGItOTA0MzBjZTI3ZDYyXkEyXkFqcGdeQXVyMTQ3Mzk2MDg4._V1_.jpg ",
+    phone: "+91 0000000000",
+    email: "yash@gmail.com"
+  },
+  {
+    name: "Priyanka Mohan",
+    imgURL:
+      "https://m.media-amazon.com/images/M/MV5BZTVmYWQ2ZTMtN2JiMi00NGZkLWJjNTItM2QyYWI5NWNmZDg5XkEyXkFqcGdeQXVyMzYxOTQ3MDg@._V1_FMjpg_UX1000_.jpg",
+    phone: "+91 0000000000",
+    email: "priyanka@gmail.com"
+  },
+  
+];
+
+export default contacts;
